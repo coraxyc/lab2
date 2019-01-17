d68fcc98023cc0979e5f263d846b161b71403231
coraxyc <ycxing99@gmail.com>
2019-01-17 00:27:06 -0800

    Added JavaScript

---
+++ static/js/introHCI.js
@@ -0,0 +1,16 @@
+function initializePage() {
+    $("#testjs").click(function(e) {
+      $('.jumbotron h1').text("JavaScript is connected");
+    });
+
+    $("a.thumbnail").click(projectClick);
+}
+
+
+function projectClick(e) {
+  //prevent the page from reloading
+  e.preventDefault();
+  // In an event handler, $(this) refers to 
+  // the object that triggered the event 
+  $(this).css("background-color", "#7fff00");
+}
